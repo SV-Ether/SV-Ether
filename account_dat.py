60f9a5c4d98fb96f23dead288c5f376ef9bb6ac2
SV-Ether <90668055+SV-Ether@users.noreply.github.com>
2021-09-14 09:18:45 +0530

    Create account_dat.py

---
+++ account_dat.py
@@ -0,0 +1,26 @@
+import pickle
+
+def createFile():
+    act_no = input('Enter account No.: ')
+    acc_type = input('Enter account type: ')
+    acc_name = input('Enter account name: ')
+    acc_bal = input('Enter account bal: ')
+    data = { 'act_no': act_no, 'acc_type': acc_type, 'acc_name': acc_name, 'acc_bal': acc_bal }
+    with open('Account.dat', 'wb') as file:
+        pickle.dump(data, file)
+
+def balance(bal):
+    data = []
+    with open('Account.dat', 'rb') as file:
+        while True:
+            try: 
+                dumped = pickle.load(file)
+                data.append(dumped)
+                if dumped is None:
+                    break
+            except:
+                break
+        print(data)
+        print('Accounts: ', ",\n".join(list(map(lambda x: str(x), filter(lambda x: int(x['acc_bal']) > bal, data)))))
+createFile()
+balance(0)
